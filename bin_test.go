259369d67f2b634aaa948968211a5b1cf2e90405
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-01-05 10:17:03 +0100

    Support query parameters by using net/url

--- bin_test.go
+++ bin_test.go
@@ -19,3 +19,21 @@ func Test_BinConstructorErrorsIfNameIsEmpty(t *testing.T) {
 
 	assert.Error(t, err)
 }
+
+func Test_BinAddEndpoint(t *testing.T) {
+	bin := &Bin{}
+	endpoint := &Endpoint{}
+
+	bin.AddEndpoint(endpoint)
+
+	assert.Equal(t, endpoint, bin.Endpoints[0])
+}
+
+func Test_BinAddGroupt(t *testing.T) {
+	bin := &Bin{}
+	group := &Group{}
+
+	bin.AddGroup(group)
+
+	assert.Equal(t, group, bin.Groups[0])
+}
